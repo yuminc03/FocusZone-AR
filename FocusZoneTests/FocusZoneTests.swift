bc6118727772b92365e902a903ae8529edb11d33
Yumin Chu <yuminc03@gmail.com>
2026-06-25 11:16:54 +0900

    setup: 프로젝트 생성

---
+++ FocusZoneTests/FocusZoneTests.swift
@@ -0,0 +1,19 @@
+//
+//  FocusZoneTests.swift
+//  FocusZoneTests
+//
+//  Created by Yumin Chu on 6/25/26.
+//
+
+import Testing
+@testable import FocusZone
+
+struct FocusZoneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
